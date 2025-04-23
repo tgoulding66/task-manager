6e039ad2d8a80c5f221c4b774f2fbce2d92f51f1
tgoulding66 <tgoulding@gmail.com>
2025-04-23 11:25:13 -0700

    Add frontend project files into /frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Container, Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container className="text-center mt-5">
+      <h1>Welcome to Task Manager</h1>
+      <Button variant="primary">Get Started</Button>
+    </Container>
+  );
+}
+
+export default App;
